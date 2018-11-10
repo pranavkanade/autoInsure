d47c33d9a87d76857a6a0ffe4d61c5b3f7853f4e
pskanade <k.pranav.apk@gmail.com>
2018-11-10 18:30:39 +0530

    Added testcase for deploying flight insurace contract using insurace company contract

--- test/Insurance.test.js
+++ test/Insurance.test.js
@@ -12,8 +12,14 @@ const compiledInsuranceCompany = require('./../Contracts/build/InsuranceCompany.
 const compiledInsuranceContract = require('./../Contracts/build/Insurance.json')
 // settle the variables needed
 let InsuranceCompany;
+let InsuranceContractAddress;
 let InsuranceContract;
 let accounts;
+
+// flight details for the testing purpose
+let flightId = 1;
+let cityFrom = 11;
+let cityTo = 22;
 
 beforeEach(
     async () => {
@@ -24,11 +30,30 @@ beforeEach(
         )
             .deploy({ data: compiledInsuranceCompany.bytecode })
             .send({ from: accounts[0], gas:'5000000'});
+
+        // register new contract for a single flight
+        await InsuranceCompany
+            .methods.registerFlight(flightId, cityFrom, cityTo)
+                .send({ from: accounts[0], gas: '5000000'});
+
+        // Find the address
+        InsuranceContractAddress = await InsuranceCompany
+            .methods.getInsuredFlight(flightId).call();
+
+        // get the instance of the deployed contract
+        InsuranceContract = await new web3.eth.Contract(
+            JSON.parse(compiledInsuranceContract.interface),
+            InsuranceContractAddress
+        );
     }
 );
 
 describe("Insurance Company", () => {
-    it("gets deployed", async () => {
+    it("Gets deployed", async () => {
         assert.ok(InsuranceCompany.options.address);
+    });
+
+    it("Can deploy insurance contract and get its address back", async () => {
+        assert.ok(InsuranceContract);
     });
 });
